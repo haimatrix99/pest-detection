541ad893781da30003e541780ae258fea9f407f4
Lê Trung Hải <80798862+haimatrix99@users.noreply.github.com>
2022-06-10 14:44:51 +0700

    test video

--- object_detection_tflite/main.py
+++ object_detection_tflite/main.py
@@ -1,5 +1,23 @@
+from imutils.video import FileVideoStream
 import torch
+import cv2
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='best-fp16.tflite')  # TFLite
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='assets/best-fp32.tflite')
+
+cap = FileVideoStream("data/VID-01.mp4").start()
+
+while True:
+    frame = cap.read()
+    
+    frame_resized = cv2.resize(frame, (640,640))
+    frame_pred = model(frame_resized, size=640)
+    frame_pred.render()
+    cv2.imshow("Video", frame_resized)
+    if cv2.waitKey(1) == ord("q"):
+        break
+    
+cv2.destroyAllWindows()
+cap.stop()
+
 
 
