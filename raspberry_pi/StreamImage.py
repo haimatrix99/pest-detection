b725be112bea679a233b723c4527f16a3ae4f344
Lê Trung Hải <80798862+haimatrix99@users.noreply.github.com>
2022-06-19 11:19:53 +0700

    fix bug

--- raspberry_pi/StreamImage.py
+++ raspberry_pi/StreamImage.py
@@ -105,7 +105,7 @@ class StreamImage:
                 try:
                     if self.annotate and response != []:
                         self.__annotate(preprocessedImage, response)
-                        self.displayImage = cv2.imencode('.jpg', preprocessedImage)[1].tobytes()
+                    self.displayImage = cv2.imencode('.jpg', preprocessedImage)[1].tobytes()
                 except Exception as e:
                     print("Could not display the video to a web browser.") 
                     print('Excpetion -' + str(e))
